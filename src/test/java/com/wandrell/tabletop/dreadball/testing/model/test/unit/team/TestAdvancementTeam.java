d9b3a18f7eec02d1dc41147e41ce3d0525f76c8c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-12-08 23:31:25 +0100

    Style corrections.

--- src/test/java/com/wandrell/tabletop/dreadball/testing/model/test/unit/team/TestAdvancementTeam.java
+++ src/test/java/com/wandrell/tabletop/dreadball/testing/model/test/unit/team/TestAdvancementTeam.java
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package com.wandrell.tabletop.dreadball.testing.model.test.unit.team;
 
 import org.mockito.Mockito;
