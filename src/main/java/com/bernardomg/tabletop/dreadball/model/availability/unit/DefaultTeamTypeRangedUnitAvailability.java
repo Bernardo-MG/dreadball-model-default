a9f3cf26911a9345cf0affa42cf01665e7504247
Bernardo <programming@wandrell.com>
2017-11-21 23:20:04 +0100

    Relocation

---
+++ src/main/java/com/bernardomg/tabletop/dreadball/model/availability/unit/DefaultTeamTypeRangedUnitAvailability.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright 2015-2016 the original author or authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.bernardomg.tabletop.dreadball.model.availability.unit;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.bernardomg.tabletop.dreadball.model.availability.unit.TeamTypeRangedUnitAvailability;
+import com.bernardomg.tabletop.dreadball.model.faction.TeamType;
+import com.bernardomg.tabletop.dreadball.model.unit.Unit;
+
+/**
+ * Unit availabilities for a
+ * {@link com.bernardomg.tabletop.dreadball.model.faction.TeamType TeamType},
+ * where there is a range of how many times it can be acquired.
+ * <p>
+ * This is an immutable implementation.
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ */
+public final class DefaultTeamTypeRangedUnitAvailability
+        implements TeamTypeRangedUnitAvailability, Serializable {
+
+    /**
+     * Serialization id.
+     */
+    private static final long serialVersionUID = 1431974307074955982L;
+
+    /**
+     * Initial number of units of this type for the team type.
+     */
+    private final Integer     avaInitial;
+
+    /**
+     * Maximum number of units of this type for the team type.
+     */
+    private final Integer     avaMax;
+
+    /**
+     * {@code Unit} for which the availability applies.
+     */
+    private final Unit        avaUnit;
+
+    /**
+     * {@code TeamType} for which the availability applies.
+     */
+    private final TeamType    teamType;
+
+    /**
+     * Constructs a ranged unit availability for the specified team and unit.
+     * 
+     * @param team
+     *            {@code TeamType} for which the availability applies
+     * @param unit
+     *            {@code Unit} for which the availability applies
+     * @param initial
+     *            initial number of units of this type
+     * @param max
+     *            maximum number of units of this type
+     */
+    public DefaultTeamTypeRangedUnitAvailability(final TeamType team,
+            final Unit unit, final Integer initial, final Integer max) {
+        super();
+
+        teamType = checkNotNull(team,
+                "Received a null pointer as the team type");
+        avaUnit = checkNotNull(unit, "Received a null pointer as the unit");
+        avaInitial = checkNotNull(initial,
+                "Received a null pointer as the initial number of units");
+        avaMax = checkNotNull(max,
+                "Received a null pointer as the maximum number of units");
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final DefaultTeamTypeRangedUnitAvailability other;
+
+        other = (DefaultTeamTypeRangedUnitAvailability) obj;
+        return Objects.equals(avaUnit, other.avaUnit);
+    }
+
+    @Override
+    public final Integer getInitialNumber() {
+        return avaInitial;
+    }
+
+    @Override
+    public final Integer getMaxNumber() {
+        return avaMax;
+    }
+
+    @Override
+    public final TeamType getTeamType() {
+        return teamType;
+    }
+
+    @Override
+    public final Unit getUnit() {
+        return avaUnit;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(avaUnit);
+    }
+
+}
---
+++ src/test/java/com/bernardomg/tabletop/dreadball/model/test/unit/team/TestDefaultSponsorTeam.java
@@ -0,0 +1,278 @@
+/**
+ * Copyright 2015-2016 the original author or authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.bernardomg.tabletop.dreadball.model.test.unit.team;
+
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.bernardomg.tabletop.dreadball.model.team.DefaultSponsorTeam;
+import com.bernardomg.tabletop.dreadball.model.team.calculator.CostCalculator;
+import com.bernardomg.tabletop.dreadball.model.faction.Sponsor;
+import com.bernardomg.tabletop.dreadball.model.team.SponsorTeam;
+import com.bernardomg.tabletop.dreadball.model.unit.Unit;
+
+/**
+ * Unit tests for {@link SponsorTeam}.
+ * <p>
+ * Checks the following cases:
+ * <ol>
+ * <li>Adding units to an empty team work as expected</li>
+ * <li>Adding a unit to an existing position overwrites the unit</li>
+ * <li>Adding a unit without giving a position uses the first empty position
+ * </li>
+ * <li>Adding a unit without giving a position works when there are no units
+ * </li>
+ * <li>Adding a unit without giving a position adds correctly to the last
+ * position</li>
+ * <li>Removing a unit works as expected</li>
+ * </ol>
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ */
+public final class TestDefaultSponsorTeam {
+
+    /**
+     * Default constructor.
+     */
+    public TestDefaultSponsorTeam() {
+        super();
+    }
+
+    /**
+     * Tests that adding a unit without giving a position works when there are
+     * no units.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testAddPlayer_AutoPos_Empty() {
+        final SponsorTeam team; // Tested team
+        final Unit player1;     // Mocked player 1
+        final Unit player2;     // Mocked player 2
+        final Sponsor sponsor;  // Mocked sponsor
+        final CostCalculator<SponsorTeam> calculator; // Mocked
+                                                      // calculator
+        final CostCalculator<SponsorTeam> ranker; // Mocked rank calculator
+
+        // Mocks sponsor
+        sponsor = Mockito.mock(Sponsor.class);
+
+        // Mocks calculators
+        calculator = Mockito.mock(CostCalculator.class);
+        ranker = Mockito.mock(CostCalculator.class);
+
+        // Creates team
+        team = new DefaultSponsorTeam(sponsor, calculator, ranker);
+
+        // Mocks players
+        player1 = Mockito.mock(Unit.class);
+        player2 = Mockito.mock(Unit.class);
+
+        // Adds player
+        team.addPlayer(player1);
+        team.addPlayer(player2);
+
+        Assert.assertEquals(team.getPlayers().get(1), player1);
+    }
+
+    /**
+     * Tests that adding a unit without giving a position uses the first empty
+     * position.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testAddPlayer_AutoPos_FirstEmpty() {
+        final SponsorTeam team; // Tested team
+        final Unit player1;     // Mocked player 1
+        final Unit player2;     // Mocked player 2
+        final Unit player3;     // Mocked player 3
+        final Sponsor sponsor;  // Mocked sponsor
+        final CostCalculator<SponsorTeam> calculator; // Mocked
+                                                      // calculator
+        final CostCalculator<SponsorTeam> ranker; // Mocked rank calculator
+
+        // Mocks sponsor
+        sponsor = Mockito.mock(Sponsor.class);
+
+        // Mocks calculators
+        calculator = Mockito.mock(CostCalculator.class);
+        ranker = Mockito.mock(CostCalculator.class);
+
+        // Creates team
+        team = new DefaultSponsorTeam(sponsor, calculator, ranker);
+
+        // Mocks players
+        player1 = Mockito.mock(Unit.class);
+        player2 = Mockito.mock(Unit.class);
+        player3 = Mockito.mock(Unit.class);
+
+        // Adds players
+        team.addPlayer(player1, 1);
+        team.addPlayer(player3, 3);
+
+        team.addPlayer(player2);
+
+        Assert.assertEquals(team.getPlayers().get(2), player2);
+    }
+
+    /**
+     * Tests that adding a unit without giving a position adds correctly to the
+     * last position.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testAddPlayer_AutoPos_LastPos() {
+        final SponsorTeam team; // Tested team
+        final Unit player1;     // Mocked player 1
+        final Unit player2;     // Mocked player 2
+        final Unit player3;     // Mocked player 3
+        final Sponsor sponsor;  // Mocked sponsor
+        final CostCalculator<SponsorTeam> calculator; // Mocked
+                                                      // calculator
+        final CostCalculator<SponsorTeam> ranker; // Mocked rank calculator
+
+        // Mocks sponsor
+        sponsor = Mockito.mock(Sponsor.class);
+
+        // Mocks calculators
+        calculator = Mockito.mock(CostCalculator.class);
+        ranker = Mockito.mock(CostCalculator.class);
+
+        // Creates team
+        team = new DefaultSponsorTeam(sponsor, calculator, ranker);
+
+        // Mocks players
+        player1 = Mockito.mock(Unit.class);
+        player2 = Mockito.mock(Unit.class);
+        player3 = Mockito.mock(Unit.class);
+
+        // Adds player
+        team.addPlayer(player1);
+        team.addPlayer(player2);
+        team.addPlayer(player3);
+
+        Assert.assertEquals(team.getPlayers().get(3), player3);
+    }
+
+    /**
+     * Tests that adding units to an empty team work as expected.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testAddPlayer_Position_Empty() {
+        final SponsorTeam team; // Tested team
+        final Unit player1;     // Mocked player 1
+        final Unit player2;     // Mocked player 2
+        final Sponsor sponsor;  // Mocked sponsor
+        final CostCalculator<SponsorTeam> calculator; // Mocked
+                                                      // calculator
+        final CostCalculator<SponsorTeam> ranker; // Mocked rank calculator
+
+        // Mocks sponsor
+        sponsor = Mockito.mock(Sponsor.class);
+
+        // Mocks calculators
+        calculator = Mockito.mock(CostCalculator.class);
+        ranker = Mockito.mock(CostCalculator.class);
+
+        // Creates team
+        team = new DefaultSponsorTeam(sponsor, calculator, ranker);
+
+        // Mocks players
+        player1 = Mockito.mock(Unit.class);
+        player2 = Mockito.mock(Unit.class);
+
+        // Adds players
+        team.addPlayer(player1, 1);
+        team.addPlayer(player2, 3);
+
+        Assert.assertEquals(team.getPlayers().size(), 2);
+    }
+
+    /**
+     * Tests that adding a unit to an existing position overwrites the unit.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testAddPlayer_Position_Overwrite() {
+        final SponsorTeam team; // Tested team
+        final Unit player1;     // Mocked player 1
+        final Unit player2;     // Mocked player 2
+        final Sponsor sponsor;  // Mocked sponsor
+        final CostCalculator<SponsorTeam> calculator; // Mocked
+                                                      // calculator
+        final CostCalculator<SponsorTeam> ranker; // Mocked rank calculator
+
+        // Mocks sponsor
+        sponsor = Mockito.mock(Sponsor.class);
+
+        // Mocks calculators
+        calculator = Mockito.mock(CostCalculator.class);
+        ranker = Mockito.mock(CostCalculator.class);
+
+        // Creates team
+        team = new DefaultSponsorTeam(sponsor, calculator, ranker);
+
+        // Mocks players
+        player1 = Mockito.mock(Unit.class);
+        player2 = Mockito.mock(Unit.class);
+
+        // Adds players
+        team.addPlayer(player1, 1);
+        team.addPlayer(player2, 1);
+
+        Assert.assertEquals(team.getPlayers().size(), 1);
+
+        Assert.assertTrue(
+                team.getPlayers().values().iterator().next() != player1);
+        Assert.assertTrue(
+                team.getPlayers().values().iterator().next() == player2);
+    }
+
+    /**
+     * Tests that removing a unit using its position works as expected.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public final void testRemovePlayer() {
+        final SponsorTeam team; // Tested team
+        final Unit player;      // Mocked player
+        final Sponsor sponsor;  // Mocked sponsor
+        final CostCalculator<SponsorTeam> calculator; // Mocked
+                                                      // calculator
+        final CostCalculator<SponsorTeam> ranker; // Mocked rank calculator
+
+        // Mocks sponsor
+        sponsor = Mockito.mock(Sponsor.class);
+
+        // Mocks calculators
+        calculator = Mockito.mock(CostCalculator.class);
+        ranker = Mockito.mock(CostCalculator.class);
+
+        // Creates team
+        team = new DefaultSponsorTeam(sponsor, calculator, ranker);
+
+        // Mocks player
+        player = Mockito.mock(Unit.class);
+        team.addPlayer(player, 1);
+
+        team.removePlayer(1);
+
+        Assert.assertEquals(team.getPlayers().size(), 0);
+    }
+
+}
